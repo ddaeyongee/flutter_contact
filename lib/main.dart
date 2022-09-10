00102ba4d764a6626428abe97460cc0dc720055d
taeyongkang <ddaeyongee@gmail.com>
2022-09-10 13:25:41 +0900

    flutter basic widget - create ListView.builder & FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());  // const 는 상수 선언이기 때문에 굳이 필요 없음
 }
+
+
+var  a = 1 ;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,24 +13,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Text(a.toString()),
+            onPressed: (){
+              print(a);
+              a++;
+            },
+          ),
           appBar: AppBar(
-            title: Text('테스트중입니다'),
+            title: Text('테스트페이지 appBar'),
             leading: Icon(Icons.star)
           ),
-          body: ShopItem(),
+          body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (c, i){
+              print(i);
+              return ListTile(
+                leading: Image.asset('selfy.jpg'),
+                title: Text('반복테스트중'),
+              );
+            },
+          ),
         )
     );
 
-  }
-}
-
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
-
-  @override
-  build(context) {
-    return SizedBox(
-      child: Text('안녕'),
-    );
   }
 }
